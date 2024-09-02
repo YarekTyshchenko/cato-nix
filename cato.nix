ee1c46e919eeb699527691ffff366e9b02f28b45
Yarek <yarek@harksys.com>
2024-09-02 13:52:33 +0100

    Make work!
    Thanks adisladis from Discord

--- cato.nix
+++ cato.nix
@@ -8,37 +8,38 @@
   autoPatchelfHook,
 }:
 
-let
-  rpath = lib.makeLibraryPath [
-    libz
-  ] + ":${stdenv.cc.cc.lib}/lib64";
-
-in stdenv.mkDerivation {
+stdenv.mkDerivation {
   pname = "cato";
-  version = "0.1.0";
+  version = "0.0.0";
 
   src = fetchurl {
     url = "https://clientdownload.catonetworks.com/public/clients/cato-client-install.deb";
     sha256 = "sha256-0hUchaxaiKJth2ByQMFfjsCLi/4kl+SrNSQ33Y6r3WA=";
   };
+
   unpackPhase = "true";
 
+  nativeBuildInputs = [ autoPatchelfHook ];
+
   buildInputs = [
+    stdenv.cc.cc.lib
     libz
+
     dpkg
     tree
   ];
 
   installPhase = ''
+    runHook preInstall
+
     mkdir -p $out
     dpkg -x $src $out
 
     tree $out
 
     cp -av $out/usr/* $out
-  '';
 
-  postFixup = ''
+    runHook postInstall
   '';
 
   meta = with lib; {
